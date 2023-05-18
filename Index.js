128bde7dffffd01b004656f6e69bcee6e8fa07a3
AidanLod <alodwig@carthage.edu>
2023-05-18 10:03:16 -0500

    incorporating the ability to use the google drive api

--- Index.js
+++ Index.js
@@ -18,7 +18,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); 
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
+const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly',
+'https://mail.google.com/',
+'https://www.googleapis.com/auth/drive.metadata.readonly'];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
