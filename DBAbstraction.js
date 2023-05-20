34d52fa6f1120374f8a64cc5e44c43f7a80b25c4
Noah Wilson <nwilson0603@gmail.com>
2023-05-20 00:15:10 -0500

    Deleted html table, more database retrieval stuff

--- DBAbstraction.js
+++ DBAbstraction.js
@@ -1,330 +1,351 @@
-const sqlite3 = require('sqlite3'); 
+const sqlite3 = require('sqlite3');
  
-class DBAbstraction { 
-    constructor(fileName) { 
-        this.fileName = fileName; 
-    } 
-    init() { 
-        return new Promise((resolve, reject) => { 
-            this.db = new sqlite3.Database(this.fileName, (err) => { 
-                if(err) { 
-                    reject(err); 
-                } else { 
-                    resolve(); 
-                } 
-            }); 
-        });
-    } 
+class DBAbstraction {
+	constructor(fileName) {
+    	this.fileName = fileName;
+	}
+	init() {
+    	return new Promise((resolve, reject) => {
+        	this.db = new sqlite3.Database(this.fileName, (err) => {
+            	if(err) {
+                	reject(err);
+            	} else {
+                	resolve();
+            	}
+        	});
+    	});
+	}
  
 
-    insertCompany(cname, street, city, state, zip, first, last, phone, email, web) 
-    {
-        const sql = 'INSERT INTO Company (name, street, city, state, zip, ContactFirst, ContactLast, phone, email, companyWeb) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);';
-        return new Promise((resolve, reject) => { 
-            this.db.run(sql, [cname, street, city, state, zip, first, last, phone, email, web], (err) => {                 
-                if(err) { 
-                    reject(err); 
-                } else { 
-                    resolve(); 
-                } 
-            }); 
-        }); 
-    }
+	insertCompany(cname, street, city, state, zip, first, last, phone, email, web)
+	{
+    	const sql = 'INSERT INTO Company (name, street, city, state, zip, ContactFirst, ContactLast, phone, email, companyWeb) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);';
+    	return new Promise((resolve, reject) => {
+        	this.db.run(sql, [cname, street, city, state, zip, first, last, phone, email, web], (err) => {            	 
+            	if(err) {
+                	reject(err);
+            	} else {
+                	resolve();
+            	}
+        	});
+    	});
+	}
 
-    insertDepartment(dname, head)
-    {
-        const sql = 'INSERT INTO Department ( name, head) VALUES (?, ?, ?);';
-        return new Promise((resolve, reject) => { 
-            this.db.run(sql, [dname, head], (err) => {                 
-                if(err) { 
-                    reject(err); 
-                } else { 
-                    resolve(); 
-                } 
-            }); 
-        }); 
-    }
+	insertDepartment(dname, head)
+	{
+    	const sql = 'INSERT INTO Department (name, head) VALUES (?, ?, ?);';
+    	return new Promise((resolve, reject) => {
+        	this.db.run(sql, [dname, head], (err) => {            	 
+            	if(err) {
+                	reject(err);
+            	} else {
+                	resolve();
+            	}
+        	});
+    	});
+	}
 
-    insertProject(Description, pstatus, file) 
-    {
-        const sql = 'INSERT INTO Project (Description, pstatus, file) VALUES (?, ?, ?);';
-        return new Promise((resolve, reject) => { 
-            this.db.run(sql, [Description, pstatus, file], (err) => {                 
-                if(err) { 
-                    reject(err); 
-                } else { 
-                    resolve(); 
-                } 
-            }); 
-        }); 
-    }
+	insertProject(Description, pstatus, file)
+	{
+    	const sql = 'INSERT INTO Project (Description, pstatus, file) VALUES (?, ?, ?);';
+    	return new Promise((resolve, reject) => {
+        	this.db.run(sql, [Description, pstatus, file], (err) => {            	 
+            	if(err) {
+                	reject(err);
+            	} else {
+                	resolve();
+            	}
+        	});
+    	});
+	}
 
-    insertUser(first, last, role, email, phone) 
-    {
-        const sql = 'INSERT INTO User (first, last, role, email, phone) VALUES (?, ?, ?, ?, ?, ?);';
-        return new Promise((resolve, reject) => { 
-            this.db.run(sql, [first, last, role, email, phone], (err) => {                 
-                if(err) { 
-                    reject(err); 
-                } else { 
-                    resolve(); 
-                } 
-            }); 
-        }); 
-    }
+	insertUser(first, last, role, email, phone)
+	{
+    	const sql = 'INSERT INTO User (first, last, role, email, phone) VALUES (?, ?, ?, ?, ?, ?);';
+    	return new Promise((resolve, reject) => {
+        	this.db.run(sql, [first, last, role, email, phone], (err) => {            	 
+            	if(err) {
+                	reject(err);
+            	} else {
+                	resolve();
+            	}
+        	});
+    	});
+	}
 
-    getAllProjects()
-    {
-        const sql = `
-            SELECT Project.Description, Company.name, Company.phone, Department.name
-            FROM Project, Company, Department
-            WHERE Project.CompanyID = Company.CompanyID AND Project.DepartmentID = Department.DepartmentID;
-        `;
+	getAllProjects()
+	{
+    	const sql = `
+        	SELECT Company.name, Company.phone, Project.Description, Department.name
+        	FROM Project, Company, Department
+        	WHERE Project.projectID = Company.companyID AND Project.projectID = Department.departmentID;
+    	`;
 
-        return new Promise((resolve, reject) => { 
-            this.db.get(sql, (err) => { 
-                if(err) { 
-                    reject(err); 
-                } else { 
-                    resolve(); 
-                } 
-            }); 
-        });
+    	return new Promise((resolve, reject) => {
+        	this.db.all(sql, (err) => {
+            	if(err) {
+                	reject(err);
+            	} else {
+                	resolve();
+            	}
+        	});
+    	});
 
-        //rewrite based on how Austin plans to restructure the Database
-    }
+    	//rewrite based on how Austin plans to restructure the Database
+	}
 
-    getCompanyByName(cname) 
-    { 
-        const sql = ` 
-            SELECT name, street, city, state, zip, rfirst, rlast, phone, email 
-            FROM Company
-            WHERE name = ? COLLATE NOCASE; 
-        `; 
-        return new Promise((resolve, reject) => { 
-            this.db.get(sql, [cname], (err, row) => {                 
-                if(err) { 
-                    reject(err); 
-                } else { 
-                    resolve(row); 
-                } 
-            }); 
-        }); 
-    } 
+    getAllInformationByProjectDescription(descrip)
+	{
+    	const sql = `
+        	SELECT Project.Description, Project.pstatus, Project.file, Department.name, Department.head, Company.name, Company.street, Company.city, Company.state, Company.zip, Company.first, Company.last, Company.phone, Company.email, Company.companyWeb, Company.ContactFirst, Company.ContactLast
+        	FROM Project, Company, Department
+        	WHERE Project.projectID = Company.companyID AND Project.projectID = Department.departmentID AND Project.Description = ? COLLATE NOCASE;
+    	`;
 
-    getCompanyByCity(city) 
-    { 
-        const sql = ` 
-            SELECT name, street, city, state, zip, ContactFirst, ContactLast, phone, email 
-            FROM Company
-            WHERE city = ? COLLATE NOCASE; 
-        `; 
-        return new Promise((resolve, reject) => { 
-            this.db.get(sql, [city], (err, row) => {                 
-                if(err) { 
-                    reject(err); 
-                } else { 
-                    resolve(row); 
-                } 
-            }); 
-        }); 
-    } 
+    	return new Promise((resolve, reject) => {
+        	this.db.get(sql, [descrip], (err, row) => {
+            	if(err) {
+                	reject(err);
+            	} else {
+                	resolve(row);
+            	}
+        	});
+    	});
 
-    getUserByLastName(last) 
-    { 
-        const sql = ` 
-            SELECT first, last, role, email, phone
-            FROM User
-            WHERE last = ? COLLATE NOCASE; 
-        `; 
-        return new Promise((resolve, reject) => { 
-            this.db.get(sql, [last], (err, row) => {                 
-                if(err) { 
-                    reject(err); 
-                } else { 
-                    resolve(row); 
-                } 
-            }); 
-        }); 
-    } 
+    	//rewrite based on how much information Austin wants the right-hand generated tables to contain
+	}
 
-    getCompanyByID(id) 
-    { 
-        const sql = ` 
-            SELECT name, street, city, state, zip, rfirst, rlast, phone, email 
-            FROM Company
-            WHERE companyID = ? COLLATE NOCASE; 
-        `; 
-        return new Promise((resolve, reject) => { 
-            this.db.get(sql, [id], (err, row) => {                 
-                if(err) { 
-                    reject(err); 
-                } else { 
-                    resolve(row); 
-                } 
-            }); 
-        }); 
-    } 
+	getCompanyByName(cname)
+	{
+    	const sql = `
+        	SELECT name, street, city, state, zip, rfirst, rlast, phone, email
+        	FROM Company
+        	WHERE name = ? COLLATE NOCASE;
+    	`;
+    	return new Promise((resolve, reject) => {
+        	this.db.get(sql, [cname], (err, row) => {            	 
+            	if(err) {
+                	reject(err);
+            	} else {
+                	resolve(row);
+            	}
+        	});
+    	});
+	}
 
-    getUserByid(id) 
-    { 
-        const sql = ` 
-            SELECT first, last, role, email, phone
-            FROM User
-            WHERE userID = ? COLLATE NOCASE; 
-        `; 
-        return new Promise((resolve, reject) => { 
-            this.db.get(sql, [id], (err, row) => {                 
-                if(err) { 
-                    reject(err); 
-                } else { 
-                    resolve(row); 
-                } 
-            }); 
-        }); 
-    } 
+	getCompanyByCity(city)
+	{
+    	const sql = `
+        	SELECT name, street, city, state, zip, ContactFirst, ContactLast, phone, email
+        	FROM Company
+        	WHERE city = ? COLLATE NOCASE;
+    	`;
+    	return new Promise((resolve, reject) => {
+        	this.db.get(sql, [city], (err, row) => {            	 
+            	if(err) {
+                	reject(err);
+            	} else {
+                	resolve(row);
+            	}
+        	});
+    	});
+	}
 
-    getProjectCompanyID(id) 
-    { 
-        const sql = ` 
-            SELECT companyID
-            FROM ProjectInfo
-            WHERE projectID = ? COLLATE NOCASE; 
-        `; 
-        return new Promise((resolve, reject) => { 
-            this.db.get(sql, [id], (err, row) => {                 
-                if(err) { 
-                    reject(err); 
-                } else { 
-                    resolve(row); 
-                } 
-            }); 
-        }); 
-    } 
+	getUserByLastName(last)
+	{
+    	const sql = `
+        	SELECT first, last, role, email, phone
+        	FROM User
+        	WHERE last = ? COLLATE NOCASE;
+    	`;
+    	return new Promise((resolve, reject) => {
+        	this.db.get(sql, [last], (err, row) => {            	 
+            	if(err) {
+                	reject(err);
+            	} else {
+                	resolve(row);
+            	}
+        	});
+    	});
+	}
 
-    getCompanyProjectID(id) 
-    { 
-        const sql = ` 
-            SELECT projectID
-            FROM ProjectInfo
-            WHERE companyID = ? COLLATE NOCASE; 
-        `; 
-        return new Promise((resolve, reject) => { 
-            this.db.get(sql, [id], (err, row) => {                 
-                if(err) { 
-                    reject(err); 
-                } else { 
-                    resolve(row); 
-                } 
-            }); 
-        }); 
-    } 
+	getCompanyByID(id)
+	{
+    	const sql = `
+        	SELECT name, street, city, state, zip, rfirst, rlast, phone, email
+        	FROM Company
+        	WHERE companyID = ? COLLATE NOCASE;
+    	`;
+    	return new Promise((resolve, reject) => {
+        	this.db.get(sql, [id], (err, row) => {            	 
+            	if(err) {
+                	reject(err);
+            	} else {
+                	resolve(row);
+            	}
+        	});
+    	});
+	}
 
-    getProjectDepartmentID(id)
-    {
-        const sql = `
-            SELECT projectID
-            FROM ProjectDepartment
-            WHERE departmentID = ? COLLATE NOCASE;
-        `;
-        return new Promise((resolve, reject) => {
-            this.db.get(sql, [id], (err, row) => {
-                if(err) {
-                    reject(err);
-                } else {
-                    resolve(row);
-                }
-            });
-        });
-    }
-    getDepartmentProjectID(id)
-    {
-        const sql = `
-            SELECT departmentID
-            FROM ProjectDepartment
-            WHERE projectID = ? COLLATE NOCASE;
-        `;
-        return new Promise((resolve, reject) => {
-            this.db.get(sql, [id], (err, row) => {
-                if(err) {
-                    reject(err);
-                } else {
-                    resolve(row);
-                }
-            });
-        });
-    }
+	getUserByid(id)
+	{
+    	const sql = `
+        	SELECT first, last, role, email, phone
+        	FROM User
+        	WHERE userID = ? COLLATE NOCASE;
+    	`;
+    	return new Promise((resolve, reject) => {
+        	this.db.get(sql, [id], (err, row) => {            	 
+            	if(err) {
+                	reject(err);
+            	} else {
+                	resolve(row);
+            	}
+        	});
+    	});
+	}
 
-    getDepartmentAssociationDID(id)
-    {
-        const sql = `
-            SELECT departmentID
-            FROM DepartmentAssociation
-            WHERE userID = ? COLLATE NOCASE;
-        `;
-        return new Promise((resolve, reject) => {
-            this.db.get(sql, [id], (err, row) => {
-                if(err) {
-                    reject(err);
-                } else {
-                    resolve(row);
-                }
-            });
-        });
-    }
+	getProjectCompanyID(id)
+	{
+    	const sql = `
+        	SELECT companyID
+        	FROM ProjectInfo
+        	WHERE projectID = ? COLLATE NOCASE;
+    	`;
+    	return new Promise((resolve, reject) => {
+        	this.db.get(sql, [id], (err, row) => {            	 
+            	if(err) {
+                	reject(err);
+            	} else {
+                	resolve(row);
+            	}
+        	});
+    	});
+	}
 
-    getDepartmentAssociationUID(id)
-    {
-        const sql = `
-            SELECT userID
-            FROM DepartmentAssociation
-            WHERE departmentID = ? COLLATE NOCASE;
-        `;
-        return new Promise((resolve, reject) => {
-            this.db.get(sql, [id], (err, row) => {
-                if(err) {
-                    reject(err);
-                } else {
-                    resolve(row);
-                }
-            });
-        });
-    }
+	getCompanyProjectID(id)
+	{
+    	const sql = `
+        	SELECT projectID
+        	FROM ProjectInfo
+        	WHERE companyID = ? COLLATE NOCASE;
+    	`;
+    	return new Promise((resolve, reject) => {
+        	this.db.get(sql, [id], (err, row) => {            	 
+            	if(err) {
+                	reject(err);
+            	} else {
+                	resolve(row);
+            	}
+        	});
+    	});
+	}
 
-    getUserProjectUID(id)
-    {
-        const sql = `
-            SELECT userID
-            FROM UserProject
-            WHERE projectID = ? COLLATE NOCASE;
-        `;
-        return new Promise((resolve, reject) => {
-            this.db.get(sql, [id], (err, row) => {
-                if(err) {
-                    reject(err);
-                } else {
-                    resolve(row);
-                }
-            });
-        });
-    }
+	getProjectDepartmentID(id)
+	{
+    	const sql = `
+        	SELECT projectID
+        	FROM ProjectDepartment
+        	WHERE departmentID = ? COLLATE NOCASE;
+    	`;
+    	return new Promise((resolve, reject) => {
+        	this.db.get(sql, [id], (err, row) => {
+            	if(err) {
+                	reject(err);
+            	} else {
+                	resolve(row);
+            	}
+        	});
+    	});
+	}
+	getDepartmentProjectID(id)
+	{
+    	const sql = `
+        	SELECT departmentID
+        	FROM ProjectDepartment
+        	WHERE projectID = ? COLLATE NOCASE;
+    	`;
+    	return new Promise((resolve, reject) => {
+        	this.db.get(sql, [id], (err, row) => {
+            	if(err) {
+                	reject(err);
+            	} else {
+                	resolve(row);
+            	}
+        	});
+    	});
+	}
 
-    getUserProjectPID(id)
-    {
-        const sql = `
-            SELECT projectID
-            FROM UserProject
-            WHERE userID = ? COLLATE NOCASE;
-        `;
-        return new Promise((resolve, reject) => {
-            this.db.get(sql, [id], (err, row) => {
-                if(err) {
-                    reject(err);
-                } else {
-                    resolve(row);
-                }
-            });
-        });
-    }
+	getDepartmentAssociationDID(id)
+	{
+    	const sql = `
+        	SELECT departmentID
+        	FROM DepartmentAssociation
+        	WHERE userID = ? COLLATE NOCASE;
+    	`;
+    	return new Promise((resolve, reject) => {
+        	this.db.get(sql, [id], (err, row) => {
+            	if(err) {
+                	reject(err);
+            	} else {
+                	resolve(row);
+            	}
+        	});
+    	});
+	}
+
+	getDepartmentAssociationUID(id)
+	{
+    	const sql = `
+        	SELECT userID
+        	FROM DepartmentAssociation
+        	WHERE departmentID = ? COLLATE NOCASE;
+    	`;
+    	return new Promise((resolve, reject) => {
+        	this.db.get(sql, [id], (err, row) => {
+            	if(err) {
+                	reject(err);
+            	} else {
+                	resolve(row);
+            	}
+        	});
+    	});
+	}
+
+	getUserProjectUID(id)
+	{
+    	const sql = `
+        	SELECT userID
+        	FROM UserProject
+        	WHERE projectID = ? COLLATE NOCASE;
+    	`;
+    	return new Promise((resolve, reject) => {
+        	this.db.get(sql, [id], (err, row) => {
+            	if(err) {
+                	reject(err);
+            	} else {
+                	resolve(row);
+            	}
+        	});
+    	});
+	}
+
+	getUserProjectPID(id)
+	{
+    	const sql = `
+        	SELECT projectID
+        	FROM UserProject
+        	WHERE userID = ? COLLATE NOCASE;
+    	`;
+    	return new Promise((resolve, reject) => {
+        	this.db.get(sql, [id], (err, row) => {
+            	if(err) {
+                	reject(err);
+            	} else {
+                	resolve(row);
+            	}
+        	});
+    	});
+	}
 }
 module.exports = DBAbstraction;
--- Index.js
+++ Index.js
@@ -1,24 +1,24 @@
-'use strict' 
+'use strict'
  
-const express = require('express'); 
-const morgan = require('morgan'); 
-const bodyParser = require("body-parser"); 
-const DBAbstraction = require('./DBAbstraction'); 
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require("body-parser");
+const DBAbstraction = require('./DBAbstraction');
 const fs = require('fs').promises;
 const path = require('path');
 const process = require('process');
 const {authenticate} = require('@google-cloud/local-auth');
 const {google} = require('googleapis');
-const db = new DBAbstraction('./software_Data.db'); 
+const db = new DBAbstraction('./software_Data.db');
 const mmm = require('mmmagic');
 const { constants } = require('buffer');
 const magic = new mmm.Magic(mmm.MAGIC_MIME_TYPE);
-const app = express(); 
+const app = express();
  
 app.use(morgan('dev'));
 app.use(express.static('public'));  
-app.use(bodyParser.urlencoded({ extended: false })); 
-app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://mail.google.com/',
@@ -36,11 +36,11 @@ const CREDENTIALS_PATH = path.join(process.cwd(), 'client_secret_credentials.jso
  */
 async function loadSavedCredentialsIfExist() {
   try {
-    const content = await fs.readFile(TOKEN_PATH);
-    const credentials = JSON.parse(content);
-    return google.auth.fromJSON(credentials);
+	const content = await fs.readFile(TOKEN_PATH);
+	const credentials = JSON.parse(content);
+	return google.auth.fromJSON(credentials);
   } catch (err) {
-    return null;
+	return null;
   }
 }
 
@@ -55,10 +55,10 @@ async function saveCredentials(client) {
   const keys = JSON.parse(content);
   const key = keys.installed || keys.web;
   const payload = JSON.stringify({
-    type: 'authorized_user',
-    client_id: key.client_id,
-    client_secret: key.client_secret,
-    refresh_token: client.credentials.refresh_token,
+	type: 'authorized_user',
+	client_id: key.client_id,
+	client_secret: key.client_secret,
+	refresh_token: client.credentials.refresh_token,
   });
   await fs.writeFile(TOKEN_PATH, payload);
 }
@@ -70,14 +70,14 @@ async function saveCredentials(client) {
 async function authorize() {
   let client = await loadSavedCredentialsIfExist();
   if (client) {
-    return client;
+	return client;
   }
   client = await authenticate({
-    scopes: SCOPES,
-    keyfilePath: CREDENTIALS_PATH,
+	scopes: SCOPES,
+	keyfilePath: CREDENTIALS_PATH,
   });
   if (client.credentials) {
-    await saveCredentials(client);
+	await saveCredentials(client);
   }
   return client;
 }
@@ -90,97 +90,112 @@ async function authorize() {
 async function listFiles(authClient) {
   const drive = google.drive({version: 'v3', auth: authClient});
   const res = await drive.files.list({
-    pageSize: 10,
-    fields: 'nextPageToken, files(id, name)',
+	pageSize: 10,
+	fields: 'nextPageToken, files(id, name)',
   });
   const files = res.data.files;
   if (files.length === 0) {
-    console.log('No files found.');
-    return;
+	console.log('No files found.');
+	return;
   }
 
   console.log('Files:');
   files.map((file) => {
-    console.log(`${file.name} (${file.id})`);
+	console.log(`${file.name} (${file.id})`);
   });
 }
 
 authorize().then(listFiles).catch(console.error);
 
-app.post('/project', async (req, res) => { 
-     
-    const fName = req.body.fName;
-    const lName = req.body.lName;
-    const email = req.body.email;
-    const cityTown = req.body.cityTown;
-    const OrgSite = req.body.OrgSite;
-    const pnumber = req.body.pnumber;
-    const state = req.body.state;
-    const radio = req.body.radio;
-    const OrgName = req.body.OrgName;
-    const streetAddr = req.body.streetAddr;
-    const zip = req.body.zip;
-    const helpAvail = req.body.helpAvail;
-    const Description = req.body.Description;
-    const FileDrop = req.body.FileDrop;
+app.post('/project', async (req, res) => {
+	 
+	const fName = req.body.fName;
+	const lName = req.body.lName;
+	const email = req.body.email;
+	const cityTown = req.body.cityTown;
+	const OrgSite = req.body.OrgSite;
+	const pnumber = req.body.pnumber;
+	const state = req.body.state;
+	const radio = req.body.radio;
+	const OrgName = req.body.OrgName;
+	const streetAddr = req.body.streetAddr;
+	const zip = req.body.zip;
+	const helpAvail = req.body.helpAvail;
+	const Description = req.body.Description;
+	const FileDrop = req.body.FileDrop;
 
-    await db.insertCompany(OrgName, streetAddr, cityTown, state, zip, fName, lName, pnumber, email, OrgSite);
-    await db.insertProject(Description, "Waiting", FileDrop);
+	await db.insertCompany(OrgName, streetAddr, cityTown, state, zip, fName, lName, pnumber, email, OrgSite);
+	await db.insertProject(Description, "Waiting", FileDrop);
  
-    res.json({"result": "success"}); 
+	res.json({"result": "success"});
 });
 
-app.get('/', async (req, res) => { 
-     
- /*  try { 
-    const allProjects = await db.getAllProjects(); 
-    if(allProjects) { 
-        res.json(allProjects); 
-    } else { 
-        res.json({"results": "none"}); 
-    } 
-} catch (err) { 
-    res.json({"results": "none"}); 
-} */
+app.get('/everyproject', async (req, res) => {
+	 
+   try {
+	const allProjects = await db.getAllProjects();
+	if(allProjects) {
+    	res.json(allProjects);
+	} else {
+    	res.json({"results": "none"});
+	}
+} catch (err) {
+	res.json({"results": "none"});
+}
 
-//First draft of a get function for populating the table in faculty.html. still unsure of how to call this function from the html file itself, or if im supposed to be doing that in the first place
-}); 
+//First draft of a get function for generating and populating the left-hand table in faculty.html. still unsure of how to call this function from the html file itself, or if im supposed to be doing that in the first place
+});
+
+app.get('/allinformation/:description', async (req, res) => {
+	try {
+    	const projectInfo = await db.getAllInformationByProjectDescription(req.params.description);
+    	if(projectInfo) {
+        	res.json(projectInfo);
+    	} else {
+        	res.json({"results": "none"});
+    	}
+	} catch (err) {
+    	res.json({"results": "none"});
+	}
+
+  //first draft of a get function for generating a table on the right-hand side of faculty.html with all the information about a project based on what project you clicked from the left-hand table
+});
  
-app.get('/cs-legend/:name', async (req, res) => { 
-    try { 
-        const legend = await db.getLegendByLastName(req.params.name); 
-        if(legend) { 
-            res.json(legend); 
-        } else { 
-            res.json({"results": "none"}); 
-        } 
-    } catch (err) { 
-        res.json({"results": "none"}); 
-    } 
-}); 
+app.get('/cs-legend/:name', async (req, res) => {
+	try {
+    	const legend = await db.getLegendByLastName(req.params.name);
+    	if(legend) {
+        	res.json(legend);
+    	} else {
+        	res.json({"results": "none"});
+    	}
+	} catch (err) {
+    	res.json({"results": "none"});
+	}
+});
  
-app.get('/allLegends/:year', async (req, res) => { 
-    try { 
-        const legends = await db.getAllLegendsBornOnOrAfter(Number(req.params.year)); 
-        if(legends) { 
-            res.json(legends); 
-        } else { 
-            res.json({"results": "none"}); 
-        } 
-    } catch (err) { 
-        res.json({"results": "none"}); 
-    } 
-}); 
+app.get('/allLegends/:year', async (req, res) => {
+	try {
+    	const legends = await db.getAllLegendsBornOnOrAfter(Number(req.params.year));
+    	if(legends) {
+        	res.json(legends);
+    	} else {
+        	res.json({"results": "none"});
+    	}
+	} catch (err) {
+    	res.json({"results": "none"});
+	}
+});
  
-app.use((req, res) => { 
-    res.status(404).send(`<h2>Uh Oh!</h2><p>Sorry ${req.url} cannot be found here</p>`); 
-}); 
+app.use((req, res) => {
+	res.status(404).send(`<h2>Uh Oh!</h2><p>Sorry ${req.url} cannot be found here</p>`);
+});
  
-db.init() 
-    .then(() => { 
-        app.listen(53140, () => console.log('The server is up and running...')); 
-    }) 
-    .catch(err => { 
-        console.log('Problem setting up the database'); 
-        console.log(err); 
-    });
+db.init()
+	.then(() => {
+    	app.listen(53140, () => console.log('The server is up and running...'));
+	})
+	.catch(err => {
+    	console.log('Problem setting up the database');
+    	console.log(err);
+	});
